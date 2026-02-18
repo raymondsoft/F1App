8b97ec78a5584bd165fe137b1fb9a28e27156586
Raymond GUITTONNEAU <rg1984@gmail.com>
2026-02-18 09:09:24 +0100

    initial commit

---
+++ F1App/F1AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  F1AppApp.swift
+//  F1App
+//
+//  Created by Raymond GUITTONNEAU on 18/02/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct F1AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ F1AppTests/F1AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  F1AppTests.swift
+//  F1AppTests
+//
+//  Created by Raymond GUITTONNEAU on 18/02/2026.
+//
+
+import Testing
+
+struct F1AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
