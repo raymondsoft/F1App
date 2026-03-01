054abef933c1cb876ce1f94fa0f40cd0fbb04fee
Raymond GUITTONNEAU <rg1984@gmail.com>
2026-03-01 12:39:34 +0100

    refactor: trim app container surface

--- F1App/AppCompositionRoot.swift
+++ F1App/AppCompositionRoot.swift
@@ -2,17 +2,13 @@ import F1Data
 import F1UseCases
 
 struct AppCompositionRoot {
-    let repository: JolpicaF1Repository
     let getSeasonsPageUseCase: GetSeasonsPageUseCase
-    let getRacesPageForSeasonUseCase: GetRacesPageForSeasonUseCase
     let getRacesForSeasonUseCase: GetRacesForSeasonUseCase
 
     init() {
         let repository = JolpicaF1Repository(httpClient: JolpicaHTTPClient())
 
-        self.repository = repository
         self.getSeasonsPageUseCase = GetSeasonsPageUseCase(repository: repository)
-        self.getRacesPageForSeasonUseCase = GetRacesPageForSeasonUseCase(repository: repository)
         self.getRacesForSeasonUseCase = GetRacesForSeasonUseCase(repository: repository)
     }
 }
